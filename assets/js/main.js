28bd80aa0d0c84f1077c9921192d85df46d9886d
aaronbugden210 <aaronbugden@gmail.com>
2019-09-27 15:42:51 -0400

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// // $("#carouselExampleIndicators").carousel();
+
+// $(".caroousel-item").click(function()
+// {
+//     $("#carouselExampleIndicators").carousel(1);
+// });
+
+// $("#slidePrev").click(function()
+// {
+//     $("#carouselExampleIndicators").carousel("prev");
+// });
+
+// $("#slideNext").click(function()
+// {
+//     $("#carouselExampleIndicators").carousel("next");
+// });
